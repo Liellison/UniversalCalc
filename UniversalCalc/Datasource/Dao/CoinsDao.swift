3056faad164c64393bf1230c4ee12985ec98f862
Liellison Menezes <liellison.menezes@mc1.com.br>
2021-01-24 00:22:31 -0300

    first commit

---
+++ UniversalCalc/Datasource/Dao/CoinsDao.swift
@@ -0,0 +1,8 @@
+//
+//  CoinsDao.swift
+//  UniversalCalc
+//
+//  Created by Liellison Menezes on 12/01/21.
+//
+
+import Foundation
---
+++ UniversalCalc/Datasource/Entities/CoinsTO.swift
@@ -0,0 +1,8 @@
+//
+//  CoinsTO.swift
+//  UniversalCalc
+//
+//  Created by Liellison Menezes on 12/01/21.
+//
+
+import Foundation
